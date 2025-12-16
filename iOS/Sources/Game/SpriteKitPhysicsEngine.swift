2e3d75e4b3bfc02aea43776739ea1dd7b1c9b967
kkugy <91677242+leesungkug@users.noreply.github.com>
2025-12-17 01:01:47 +0900

    feat(SpriteKitPhysicsEngine): SpriteKit의 물리엔진을 담당할 객체 구현
    중력 가속도 9.8를 적용하면 너무 미미하기에 화면 비율상 적절한 -1200을 넣었습니다. 그리고 바닥node를 만들고 엔진 영향을 받지 않도록 설정했습니다.
    
    refs DEV-10

---
+++ iOS/Sources/Game/SpriteKitPhysicsEngine.swift
@@ -0,0 +1,39 @@
+//
+//  SpriteKitPhysicsEngine.swift
+//  iOS
+//
+//  Created by sungkug_apple_developer_ac on 12/17/25.
+//
+
+import SpriteKit
+
+final class SpriteKitPhysicsEngine {
+    private unowned let scene: SKScene
+    private var nodes: [UUID: SKSpriteNode] = [:]
+    
+    init(scene: SKScene) {
+        self.scene = scene
+        configureWorld()
+        setupGround()
+    }
+    
+    private func configureWorld() {
+        // 초당 1200의 가속도가 붙음, 중력 가속도 9.8를 적용하면 너무 미미하기에 화면 비율상 적절한 값을 넣음
+        scene.physicsWorld.gravity = CGVector(dx: 0, dy: -1200)
+    }
+    
+    private func setupGround() {
+        let ground = SKNode()
+        ground.position = .zero
+
+        let y: CGFloat = 100
+
+        ground.physicsBody = SKPhysicsBody(
+            edgeFrom: CGPoint(x: 0, y: y),
+            to: CGPoint(x: scene.size.width, y: y)
+        )
+        ground.physicsBody?.isDynamic = false // 물리 시물레이션 영향을 받지 않도록 설정
+
+        scene.addChild(ground)
+    }
+}
