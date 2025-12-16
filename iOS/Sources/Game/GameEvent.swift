fdb8c0c4b3c81d715efbe704f9c11ef3a6104ca9
kkugy <91677242+leesungkug@users.noreply.github.com>
2025-12-16 21:49:39 +0900

    feat(FaceEvent, InputStrategy): 페이스 이벤트 처리를 위한 FaceEvent, FaceInputStrategy정의
    페이스 트래킹으로 인식한 값을 게임 이벤트로 치환해주기위해 GameEvent를 정의하고
    FaceInputStrategy로 프로토콜 정의하여 이후 이벤트 처리 교체하기 쉽도록 하였습니다.
    
    refs DEV-10

---
+++ iOS/Sources/Game/GameEvent.swift
@@ -0,0 +1,14 @@
+//
+//  FaceEvent.swift
+//  iOS
+//
+//  Created by sungkug_apple_developer_ac on 12/16/25.
+//
+
+import Foundation
+
+enum GameEvent {
+    case moveLeft(Double)
+    case moveRight(Double)
+    case jump(Double)
+}
