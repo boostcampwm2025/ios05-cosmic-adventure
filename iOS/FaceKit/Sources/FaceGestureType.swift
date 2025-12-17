bb1be89b4c63983e94d3dae3021303446b28daf0
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-12-18 03:21:47 +0900

    chore: 접근제어자 public으로 수정
    - GameView에서 사용하기 위함

--- iOS/FaceKit/Sources/FaceGestureType.swift
+++ iOS/FaceKit/Sources/FaceGestureType.swift
@@ -7,20 +7,14 @@
 
 import Foundation
 
-enum FaceGestureType: Equatable {
+public enum FaceGestureType: Equatable {
     case jump                   // 뽀뽀
     case superJump              // 볼에 바람 넣기
     case move(MoveDirect)       // 고개 갸웃
     case none                   // 기본 상태
 }
 
-enum MoveDirect {
+public enum MoveDirect {
     case left
     case right
-}
-
-struct FaceGestureState {
-    let gestureType: FaceGestureType
-    let intensity: Float  // 0.0~1.0
-    let timestamp: Date
 }
