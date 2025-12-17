6afe4010643a6e5953d52cb6c896fc35e684b6f6
yungu0010 <dbstj16@sookmyung.ac.kr>
2025-12-18 04:56:04 +0900

    feat: 맵 구현 및 카메라 추적 기능 구현
    - 100개 계단 디폴트로 설정
    - 벽돌에 오르면 정지되도록 player.allowsRotation = false로 두기

--- iOS/Core/Sources/Core.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public struct Core {
-    public init() {}
-}
--- iOS/Core/Sources/GameConstant.swift
+++ iOS/Core/Sources/GameConstant.swift
@@ -11,8 +11,8 @@ import Foundation
 public enum GameConstant {
     /// 동작 설정
     static public let normalJumpImpulse: CGFloat = 100
-    static public let superJumpImpulse: CGFloat = 200
-    static public let moveSpeed: CGFloat = 200
+    static public let superJumpImpulse: CGFloat = 150
+    static public let moveSpeed: CGFloat = 100
     
     /// 플레이어 설정
     static public let gravity: CGFloat = 9.8
@@ -23,4 +23,11 @@ public enum GameConstant {
     static public let jumpThreshold: Float = 0.5
     static public let superJumpThreshold: Float = 0.6
     static public let tiltThreshold: Float = 0.4
+    
+    /// 맵 설정
+    static public let brickWidth: CGFloat = 80
+    static public let brickHeight: CGFloat = 20
+    static public let startY: CGFloat = 300             // 블록 시작 높이
+    static public let verticalGap: CGFloat = 150        // 세로 간격
+    static public let numberOfLevels = 30               // 맵의 블록 수 (높게 만들기)
 }
