6c4b7f5319d6a6c0f76ab1fc1012cbc44a215a5e
kkugy <91677242+leesungkug@users.noreply.github.com>
2025-12-17 23:43:48 +0900

    feat(NetworkFrameworkGameNetworking): guest/host 로직을 전체적으로 관리하는 객체 구현
    역할(Host/Guest)이 아니라 네트워크 상태가 핵심이기에 분리하는 것보다 하나로 관리하는게 좋다고 판단했습니다.
    연결 중, 연결 후에 역할이 바뀌는 경우 객체가 분리되어있으면 오히려 복잡해진다고 생각했습니다.

---
+++ iOS/Sources/Network/NetworkFrameworkGameNetworking.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkFrameworkGameNetworking.swift
+//  iOS
+//
+//  Created by sungkug_apple_developer_ac on 12/17/25.
+//
+
+import Network
+
+final class NetworkFrameworkGameNetworking {
+    
+    // MARK: - Components
+    private var listener: NWListener?
+    private var browser: NWBrowser?
+    private var connection: NWConnection?
+}
