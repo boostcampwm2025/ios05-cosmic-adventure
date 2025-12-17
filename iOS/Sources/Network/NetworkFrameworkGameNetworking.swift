234693c820e75a6a6f63e9cde303cb9adb5abf14
kkugy <91677242+leesungkug@users.noreply.github.com>
2025-12-17 23:57:47 +0900

    feat(NetworkFrameworkGameNetworking): Public API 구현
    상태 변화 시 기존 소켓들을 초기화 하고 다시 처리할 수 있도록 구현하였습니다.

--- iOS/Sources/Network/NetworkFrameworkGameNetworking.swift
+++ iOS/Sources/Network/NetworkFrameworkGameNetworking.swift
@@ -8,9 +8,49 @@
 import Network
 
 final class NetworkFrameworkGameNetworking {
-    
+    private(set) var state: NetworkingState = .idle
+
     // MARK: - Components
     private var listener: NWListener?
     private var browser: NWBrowser?
     private var connection: NWConnection?
+    
+    // MARK: - Public API
+     func start() {
+         transition(to: .idle)
+     }
+
+     func connect(to peer: Peer) {
+         transition(to: .connecting(peer))
+     }
+
+     func disconnect() {
+         transition(to: .idle)
+     }
+}
+
+private extension NetworkFrameworkGameNetworking {
+
+    func transition(to newState: NetworkingState) {
+        cleanup()
+        state = newState
+
+        switch newState {
+        case .idle:
+
+        case .connecting(let peer):
+
+        case .connected:
+            
+        }
+    }
+
+    func cleanup() {
+        listener?.cancel()
+        browser?.cancel()
+        connection?.cancel()
+        listener = nil
+        browser = nil
+        connection = nil
+    }
 }
