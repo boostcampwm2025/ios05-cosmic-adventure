7c16a51423e352529f9cb4e0a6ae93730da616e9
kkugy <91677242+leesungkug@users.noreply.github.com>
2026-01-06 17:24:14 +0900

    feat: 외부에서 사용할 InputSystem 구현
    외부에서는 내부 InputSource 구현체를 알 필요가 없도록, convenience init에서 옵션을 받아 소스 객체를 내부에서 생성/주입하도록 구성했습니다.
    
    refs dev-18

--- iOS/Modules/InputSystem/Sources/Core/InputSystem.swift
+++ iOS/Modules/InputSystem/Sources/Core/InputSystem.swift
@@ -1,5 +1,50 @@
+
+import ARKit
 import Foundation
 
-public struct InputSystem {
-    public init() {}
+public final class InputSystem {
+    public let hub: InputEventHub
+    private let source: InputSource
+
+    public init(makeSource: (InputEventHub) -> InputSource) {
+        let hub = InputEventHub()
+        self.hub = hub
+        self.source = makeSource(hub)
+    }
+
+    public func start() { source.start() }
+    public func stop() { source.stop() }
+
+    public func events() async -> AsyncStream<InputEvent> {
+        await hub.makeStream()
+    }
+}
+
+public extension InputSystem {
+    enum FaceTrackingStrategyOption {
+        case tiltAndPucker
+        case custom(FaceInputStrategy)
+
+        var strategy: FaceInputStrategy {
+            switch self {
+            case .tiltAndPucker:
+                return TiltAndPuckerFaceInputStrategy()
+            case .custom(let s):
+                return s
+            }
+        }
+    }
+
+    enum Source {
+        case faceTracking(strategy: FaceTrackingStrategyOption = .tiltAndPucker)
+    }
+
+    convenience init(source: Source) {
+        switch source {
+        case .faceTracking(let option):
+            self.init { hub in
+                ARFaceTrackingInputSource(strategy: option.strategy, hub: hub)
+            }
+        }
+    }
 }
