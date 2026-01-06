6b866921bae2302f1fc198bedb54f4eee0d85aac
Yeongbin Jeong <oyobbeb@polygom.xyz>
2026-01-07 04:13:41 +0900

    refactor: CharacterAvatar를 Shared/Models로 분리
    하면서 BackgroundContainerView도 Shared/Components로 이동

--- iOS/App/Sources/Presentation/Lobby/LobbyModels.swift
+++ iOS/App/Sources/Presentation/Lobby/LobbyModels.swift
@@ -14,28 +14,6 @@ enum ExplorerRole: Equatable {
     case peer
 }
 
-// MARK: - Character Avatar
-
-enum CharacterAvatar: CaseIterable, Equatable {
-    case character1
-    case character2
-    case character3
-    case character4
-    case character5
-    case character6
-    
-    var image: Image {
-        switch self {
-        case .character1: AppAsset.Image.character1.swiftUIImage
-        case .character2: AppAsset.Image.character2.swiftUIImage
-        case .character3: AppAsset.Image.character3.swiftUIImage
-        case .character4: AppAsset.Image.character4.swiftUIImage
-        case .character5: AppAsset.Image.character5.swiftUIImage
-        case .character6: AppAsset.Image.character6.swiftUIImage
-        }
-    }
-}
-
 // MARK: - Lobby Explorer
 
 struct LobbyExplorer: Identifiable, Equatable {
