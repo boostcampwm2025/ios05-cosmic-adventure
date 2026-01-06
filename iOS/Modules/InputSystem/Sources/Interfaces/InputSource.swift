9a2f06a5918b3e1f899a586ddca549b78ec18a6f
kkugy <91677242+leesungkug@users.noreply.github.com>
2026-01-06 16:05:58 +0900

    feat: input값 처리할 객체 추상화

---
+++ iOS/Modules/InputSystem/Sources/Interfaces/InputSource.swift
@@ -0,0 +1,13 @@
+//
+//  InputSource.swift
+//  InputSystem
+//
+//  Created by sungkug_apple_developer_ac on 1/6/26.
+//
+
+import Foundation
+
+public protocol InputSource: AnyObject {
+    func start()
+    func stop()
+}
