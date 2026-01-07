085afd7afa2c2f37ebdb375b18501782f60963c9
kkugy <91677242+leesungkug@users.noreply.github.com>
2026-01-07 15:27:45 +0900

    feat: AppRoute 정의
    현재 흐름상 플로우가 간단하고 홈에서 대시보드나 환경설정화면은 단순 push 정도여서 서브 라우터를 두지 않았습니다.
    
    refs dev-28

---
+++ iOS/App/Sources/Application/AppFlow/AppRoute.swift
@@ -0,0 +1,21 @@
+//
+//  AppRoute.swift
+//  App
+//
+//  Created by sungkug_apple_developer_ac on 1/7/26.
+//
+
+enum AppRoute: Hashable {
+    // Onboarding
+    case permissionSetup
+    case profileSetup
+
+    // Main
+    case home
+    case settings
+    case dashboard
+
+    // Game
+    case game
+    case result
+}
