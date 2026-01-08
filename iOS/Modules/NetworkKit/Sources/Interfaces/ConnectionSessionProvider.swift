277e0f6f5d070fef1d48d94841dfc9bc3a82853a
jsu3417 <jsu3417@naver.com>
2026-01-09 01:06:54 +0900

    refactor: ConnectionSessionProvider 변경
    - onInviteReceived, onInviteResponse, onInviteCanceled 클로저 추가
    - requestInvite, replyToInvite 메소드 추가
    
    refs: DEV-21

--- iOS/Modules/NetworkKit/Sources/Interfaces/ConnectionSessionProvider.swift
+++ iOS/Modules/NetworkKit/Sources/Interfaces/ConnectionSessionProvider.swift
@@ -14,9 +14,14 @@ public enum LocalNetworkError: Error {
 
 public protocol ConnectionSessionProvider {
     var nearbyPlayer: [Peer] { get }
-    
-    var onPermissionResult: ((Result<Void, LocalNetworkError>) -> Void)? { get set }
+
+    var onLocalNetworkPermissionGranted: (() -> Void)? { get set }
+    var onLocalNetworkPermissionDenied: ((Error) -> Void)? { get set }
+    var onReceiveInvitationPacket: ((NetworkPacketType, Data) -> Void)? { get set }
+//    var onReceiveGamePacket: ((Data, Data) -> Void)? { get set }
 
     func activate(nickname: String)
     func deactive()
+    func requestInvite<T: NetworkTransferable>(to peer: Peer, packet: T)
+    func replyToInvite<T: NetworkTransferable>(to targetName: String, packet: T)
 }
