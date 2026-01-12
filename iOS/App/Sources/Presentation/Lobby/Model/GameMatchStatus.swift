bbd75a0b64196d3614b5f4559d71e016346244bd
jsu3417 <jsu3417@naver.com>
2026-01-13 02:58:51 +0900

    refactor: GameMatchStatus 변경을 함수로 호출하도록 변경
    refs: DEV-15
    
    # Conflicts:
    #	iOS/App/Sources/Presentation/Lobby/LobbyViewModel.swift

--- iOS/App/Sources/Presentation/Lobby/Model/GameMatchStatus.swift
+++ iOS/App/Sources/Presentation/Lobby/Model/GameMatchStatus.swift
@@ -14,4 +14,30 @@ enum GameMatchStatus: Equatable {
     case requestDeclined(peer: LobbyExplorer)
     case gameReady(peer: LobbyExplorer)
     case gameStart
+
+    mutating func reset() {
+        self = .idle
+    }
+
+    mutating func select(_ peer: LobbyExplorer) {
+        self = .readyToSend(peer: peer)
+    }
+
+    mutating func sendRequest() {
+        if case .readyToSend(let peer) = self {
+            self = .sendingRequest(peer: peer)
+        }
+    }
+
+    mutating func requestDeclined(by peer: LobbyExplorer) {
+        self = .requestDeclined(peer: peer)
+    }
+
+//    mutating func receiveInvite(from peer: LobbyExplorer) {
+//        self = .receivedInvite(peer: peer)
+//    }
+
+    mutating func setGameReady(with peer: LobbyExplorer) {
+        self = .gameReady(peer: peer)
+    }
 }
