b3c41ae3eed69474fc36c6eaa52ada74d095adb6
Yeongbin Jeong <113571767+oyobbeb@users.noreply.github.com>
2026-01-13 19:10:07 +0900

    Revert "chore(backend): Todo 보일러플레이트 코드 삭제"
    This reverts commit cf4277688961c57dee54ba78cd1baaaea5a1816b.

--- backend/Sources/App/configure.swift
+++ backend/Sources/App/configure.swift
@@ -5,5 +5,7 @@ import Vapor
 public func configure(_ app: Application) async throws {
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
+    app.migrations.add(CreateTodo())
+
     try routes(app)
 }
--- backend/Sources/App/routes.swift
+++ backend/Sources/App/routes.swift
@@ -1,7 +1,14 @@
+import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get("status") { _ async in
-        "Cosmic Adventure Server OK"
+    app.get { req async in
+        "It works!"
     }
+
+    app.get("hello") { req async -> String in
+        "Hello, world!"
+    }
+
+    try app.register(collection: TodoController())
 }
