719ebeab7fea4e8a1870f192539c0ed8e3fb8afd
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-01-16 05:16:14 +0900

    feat: WebSocketPlayer에 latency 속성 추가

---
+++ iOS/Modules/NetworkKit/Sources/WebSocket/WebSocketPlayer.swift
@@ -0,0 +1,19 @@
+//
+//  WebSocketPlayer.swift
+//  NetworkKit
+//
+//  Created by 강윤서 on 1/16/26.
+//
+
+
+public struct WebSocketPlayer: Identifiable, Equatable {
+    public let id: String
+    public let nickname: String
+    public var latency: Double?
+    
+    public init(id: String, nickname: String, latency: Double? = nil) {
+        self.id = id
+        self.nickname = nickname
+        self.latency = latency
+    }
+}
--- iOS/Modules/NetworkKit/Sources/WebSocket/WebSocketSessionManager.swift
+++ iOS/Modules/NetworkKit/Sources/WebSocket/WebSocketSessionManager.swift
@@ -9,18 +9,6 @@ import Foundation
 import Observation
 import os
 
-public struct WebSocketPlayer: Identifiable, Equatable {
-    public let id: String
-    public let nickname: String
-    public var latency: Double?
-    
-    public init(id: String, nickname: String, latency: Double? = nil) {
-        self.id = id
-        self.nickname = nickname
-        self.latency = latency
-    }
-}
-
 @Observable
 public final class WebSocketSessionManager: WebSocketSessionManaging {
 
