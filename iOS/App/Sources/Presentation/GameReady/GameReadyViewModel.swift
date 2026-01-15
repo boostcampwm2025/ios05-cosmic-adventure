99d6b606deac0f68ce17983707d1e3fd4bd7797d
jsu3417 <jsu3417@naver.com>
2026-01-15 20:11:00 +0900

    feat: GameReadyViewModel 로직 구현
    - checkInitialStatus(): UserDefaultsList.Game.hasSeenGameOnboarding 를 체크해서 온보딩 or gameReady 상태 데이터 송수신 처리
    - 자신과 상대방의 gameReady 상태를 모두 알기 위해 서로 데이터 송수신 과정이 필요함
        - 네트워크 연결 여부와 상관없이 모두 송수신 하기 위해 (P2P, WebSocket) NetworkSessionManager, WebSocketManager 모두 처리
        - sendReadySignal(): 자신의 gameReady 상태를 전송
        - handlePeerReady(): 상대방의 gameReady 상태를 전달받아 핸들링, ProgressBarUI 업데이트
        - GameReadyView의 attemptStart()에서 GameReadyViewModel gameReady 상태(자신과 상대방 둘다)가 변경될 때마다 체크하여 둘다 gameReady 가 되면 GameView로 전환
    
    refs: DEV-58

---
+++ iOS/App/Sources/Presentation/GameReady/GameReadyViewModel.swift
@@ -0,0 +1,165 @@
+//
+//  GameReadyViewModel.swift
+//  App
+//
+//  Created by soyoung on 1/15/26.
+//
+
+import Foundation
+import NetworkKit
+
+@MainActor
+@Observable
+final class GameReadyViewModel {
+    let me: LobbyExplorer
+    let peer: LobbyExplorer
+
+    private(set) var isMeReady: Bool = false
+    private(set) var isPeerReady: Bool = false
+
+    var showOnboarding: Bool = false
+    private(set) var message: String = ""
+    private(set) var progress: Double = 0.0
+
+    private(set) var networkMode: NetworkMode = .local
+
+    @ObservationIgnored
+    var connectivityMonitor: ConnectivityMonitoring
+
+    @ObservationIgnored
+    var networkSessionManager: NetworkSessionManaging
+
+    @ObservationIgnored
+    let webSocketSessionManager: WebSocketSessionManaging?
+
+    init(
+        me: LobbyExplorer,
+        peer: LobbyExplorer,
+        connectivityMonitor: ConnectivityMonitoring,
+        networkSessionManager: NetworkSessionManaging,
+        webSocketSessionManager: WebSocketSessionManaging?
+    ) {
+        self.me = me
+        self.peer = peer
+        self.connectivityMonitor = connectivityMonitor
+        self.networkSessionManager = networkSessionManager
+        self.webSocketSessionManager = webSocketSessionManager
+
+        setupConnectivityMonitor()
+        setupP2PCallbacks()
+        setupWebSocketCallbacks()
+    }
+
+    private func setupConnectivityMonitor() {
+        connectivityMonitor.onStatusChanged = { [weak self] isConnected in
+            Task { @MainActor in
+                self?.handleConnectivityChange(isConnected: isConnected)
+            }
+        }
+        connectivityMonitor.start()
+        networkMode = connectivityMonitor.isConnected ? .remote : .local
+    }
+
+    private func handleConnectivityChange(isConnected: Bool) {
+        if isConnected {
+            networkMode = .remote
+        } else {
+            networkMode = .local
+        }
+    }
+
+    private func setupP2PCallbacks() {
+        networkSessionManager.onReadyStatusReceived = { [weak self] senderName in
+            guard let self = self else { return }
+
+            guard senderName == self.peer.displayName else { return }
+
+            Task { @MainActor in
+                self.handlePeerReady()
+            }
+        }
+    }
+
+    private func setupWebSocketCallbacks() {
+        webSocketSessionManager?.onReadyStatusReceived = { [weak self] senderName in
+            guard let self = self else { return }
+
+            guard senderName == self.peer.displayName else { return }
+
+            Task { @MainActor in
+                self.handlePeerReady()
+            }
+        }
+    }
+
+    func checkInitialStatus() {
+        let hasSeenOnboarding = UserDefaultsList.Game.hasSeenGameOnboarding
+
+        if hasSeenOnboarding {
+            setMyReady()
+        } else {
+            showOnboarding = true
+        }
+    }
+
+    // 온보딩에서 '다시보지 않기' 또는 '닫기' 눌렀을 때 호출
+    func completeOnboarding() {
+        showOnboarding = false
+
+        // 온보딩 봤음을 저장
+        UserDefaultsList.Game.hasSeenGameOnboarding = true
+
+        setMyReady()
+    }
+
+    private func setMyReady() {
+        guard !isMeReady else { return }
+
+        isMeReady = true
+        updateProgressUI()
+
+        sendReadySignal()
+    }
+
+    private func sendReadySignal() {
+        switch networkMode {
+        case .local:
+            networkSessionManager.sendReadyStatus(to: peer.displayName)
+
+        case .remote:
+            webSocketSessionManager?.sendReadyStatus(to: peer.displayName)
+
+        }
+    }
+
+    private func handlePeerReady() {
+        guard !isPeerReady else { return }
+
+        isPeerReady = true
+        updateProgressUI()
+    }
+
+    private func updateProgressUI() {
+        // 한 명 준비되면 50%, 둘 다 되면 100%
+        if isMeReady && isPeerReady {
+            progress = 1.0
+            message = Constants.GameReady.allReadyMessage
+
+        } else if isMeReady {
+            progress = 0.5
+            message = Constants.GameReady.waitingForPeerMessage
+
+        } else if isPeerReady {
+            progress = 0.5
+            message = Constants.GameReady.peerWaitingMessage
+
+        } else {
+            progress = 0.0
+            message = Constants.GameReady.connectingMessage
+        }
+    }
+
+    func checkAllReady() -> Bool {
+        return isMeReady && isPeerReady
+    }
+}
