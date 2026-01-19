7f0b0ec72c0e8b2b04815d5cff3dc9f6ad9e34cd
jsu3417 <jsu3417@naver.com>
2026-01-19 22:23:27 +0900

    feat: VideoConfiguration, VideoEncoder, VideoDecoder 추가
    - VideoConfiguration: 기본 설정 추가
        - 해상도(PIP용 저해상도): 하드웨어 친화적인 192x192 (UI 100pt 크기)
        - bitrate(네트워크 상태에 따른 화질): 60_000, 150_000
        - motinoringInterval(화질 점검용 주기 설정) 추가: 2.0 2초에 한번씩 네트워크 상태 체크하여 bitrate를 변경
    
    - VideoEncoder: 이미지(CVPixelBuffer)를 받아서 → 압축된 데이터(H.264 Data)로 바꿔주는 역할
        - 2초마다 화면 깨짐 복구용으로 프레임 전체를 보여주는 키 프레임(I-Frame) 생성. (그 외에는 바뀐 부분만 보여주는 차이값 사용(Inter-frame))
        - 압축은 비동기 진행 후 완료시 OS가 encodingCallback을 호출.
    
    - VideoDecoder: 데이터를 받아서, 이해할 수 있는 형태(CMSampleBuffer)로 변환한 뒤, 화면 레이어에 적용하는 역할
    
    refs: DEV-70

---
+++ iOS/App/Sources/Presentation/Game/Video/VideoConfiguration.swift
@@ -0,0 +1,42 @@
+//
+//  VideoConfig.swift
+//  App
+//
+//  Created by soyoung on 1/19/26.
+//
+
+public struct VideoConfiguration {
+
+    // 해상도
+    public let width: Int32
+    public let height: Int32
+
+    // 비트레이트 (네트워크 상태에 따른 화질)
+    public let highBitrate: Int
+    public let lowBitrate: Int
+
+    // 화질 점검 및 키프레임 주기 설정 (복구용)
+    public let monitoringInterval: Double
+
+    // FPS, 키프레임 시간
+    public let frameRate: Int32
+    public let keyFrameIntervalDuration: Double
+
+    public init(
+        width: Int32 = 192,
+        height: Int32 = 192, // PIP용 저해상도
+        highBitrate: Int = 150_000,
+        lowBitrate: Int = 60_000,
+        monitoringInterval: Double = 2.0,
+        frameRate: Int32 = 30,
+        keyFrameIntervalDuration: Double = 2.0
+    ) {
+        self.width = width
+        self.height = height
+        self.highBitrate = highBitrate
+        self.lowBitrate = lowBitrate
+        self.monitoringInterval = monitoringInterval
+        self.frameRate = frameRate
+        self.keyFrameIntervalDuration = keyFrameIntervalDuration
+    }
+}
---
+++ iOS/App/Sources/Presentation/Game/Video/VideoEncoder.swift
@@ -0,0 +1,114 @@
+//
+//  VideoEncoder.swift
+//  App
+//
+//  Created by soyoung on 1/19/26.
+//
+
+import VideoToolbox
+import QuartzCore
+
+final class VideoEncoder {
+    
+    private let configuration: VideoConfiguration
+    private var session: VTCompressionSession?
+    var output: ((Data) -> Void)?
+
+    private static var encodingCallback: VTCompressionOutputCallback = { (
+        refCon: UnsafeMutableRawPointer?,
+        _: UnsafeMutableRawPointer?,
+        status: OSStatus,
+        _: VTEncodeInfoFlags,
+        sampleBuffer: CMSampleBuffer?
+    ) in
+        guard status == noErr,
+              let sampleBuffer = sampleBuffer,
+              let refCon = refCon else {
+            return
+        }
+
+        let encoder = Unmanaged<VideoEncoder>.fromOpaque(refCon).takeUnretainedValue()
+
+        if let dataBuffer = CMSampleBufferGetDataBuffer(sampleBuffer) {
+            var length = 0
+            var dataPointer: UnsafeMutablePointer<Int8>?
+
+            if CMBlockBufferGetDataPointer(
+                dataBuffer,
+                atOffset: 0,
+                lengthAtOffsetOut: nil,
+                totalLengthOut: &length,
+                dataPointerOut: &dataPointer
+            ) == kCMBlockBufferNoErr {
+                if let pointer = dataPointer {
+                    let data = Data(bytes: pointer, count: length)
+                    encoder.output?(data)
+                }
+            }
+        }
+    }
+
+    public init(configuration: VideoConfiguration = VideoConfiguration()) {
+        self.configuration = configuration
+        setupSession(initialBitrate: configuration.highBitrate)
+    }
+
+    deinit {
+        if let session = session {
+            // 대기 중인 프레임 종료
+            VTCompressionSessionCompleteFrames(session, untilPresentationTimeStamp: .invalid)
+
+            // 세션 무효화, 콜백 X
+            VTCompressionSessionInvalidate(session)
+        }
+    }
+
+    private func setupSession(initialBitrate: Int) {
+        VTCompressionSessionCreate(
+            allocator: nil,
+            width: configuration.width,
+            height: configuration.height,
+            codecType: kCMVideoCodecType_H264,
+            encoderSpecification: nil,
+            imageBufferAttributes: nil,
+            compressedDataAllocator: nil,
+            outputCallback: VideoEncoder.encodingCallback,
+            refcon: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()),
+            compressionSessionOut: &session
+        )
+
+        guard let session = session else { return }
+
+        // 2초마다 키프레임 (화면깨짐 복구용)
+        let duration = configuration.keyFrameIntervalDuration
+        let frameInterval = Int32(Double(configuration.frameRate) * duration)
+
+        VTSessionSetProperty(session, key: kVTCompressionPropertyKey_MaxKeyFrameInterval, value: frameInterval as CFNumber)
+        VTSessionSetProperty(session, key: kVTCompressionPropertyKey_MaxKeyFrameIntervalDuration, value: duration as CFNumber)
+        VTSessionSetProperty(session, key: kVTCompressionPropertyKey_RealTime, value: kCFBooleanTrue)
+        VTSessionSetProperty(session, key: kVTCompressionPropertyKey_ExpectedFrameRate, value: configuration.frameRate as CFNumber)
+
+        changeBitrate(to: initialBitrate)
+    }
+
+    func changeBitrate(to bps: Int) {
+        guard let session = session else { return }
+        VTSessionSetProperty(session, key: kVTCompressionPropertyKey_AverageBitRate, value: bps as CFNumber)
+    }
+
+    // 압축은 비동기 진행, 완료시 OS가 encodingCallback 호출
+    func encode(pixelBuffer: CVPixelBuffer) {
+        guard let session = session else { return }
+        let now = CMTime(seconds: CACurrentMediaTime(), preferredTimescale: 1000)
+
+        VTCompressionSessionEncodeFrame(
+            session,
+            imageBuffer: pixelBuffer,
+            presentationTimeStamp: now,
+            duration: .invalid,
+            frameProperties: nil,
+            sourceFrameRefcon: nil,
+            infoFlagsOut: nil
+        )
+    }
+}
