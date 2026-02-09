92704da41765af7b3acee60fd48f1a04259a4050
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-02-10 05:09:21 +0900

    fix: 게임 미리보기 pip뷰 하단 잘림 문제 수정
    refs dev-139

--- iOS/App/Sources/Presentation/Shared/UIConstants.swift
+++ iOS/App/Sources/Presentation/Shared/UIConstants.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 enum Metrics {
     // 화면 전체 사이즈 관련
+    static let screenSize = UIScreen.main.bounds.size
     static let screenWidth = UIScreen.main.bounds.width
     static let screenHeight = UIScreen.main.bounds.height
 }
